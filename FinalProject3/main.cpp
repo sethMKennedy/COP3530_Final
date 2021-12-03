52caeba2048191a9e280eded04dc4cfaba6990c6
sethmkennedy <sethkennedy@ufl.edu>
2021-12-03 17:13:31 -0500

    please God

--- FinalProject3/main.cpp
+++ FinalProject3/main.cpp
@@ -4,5 +4,6 @@ int main() {
 
 	std::cout << "I wish github was easier to use :(" << std::endl;
 	cout << "seth kennedy repo better work" << endl;
+	cout << "I think its working" << endl;
 	return 0;
 }
