a4a6a6e5a2c152da2322e164b3087674e78913f3
corygrossman <cory.grossman@ufl.edu>
2021-12-03 17:12:46 -0500

    Update main.cpp
    added some stuff

--- FinalProject3/main.cpp
+++ FinalProject3/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-
+using namespace std;
 int main() {
 
 	std::cout << "I wish github was easier to use :(" << std::endl;
+	cout << "seth kennedy repo better work" << endl;
 	return 0;
 }
